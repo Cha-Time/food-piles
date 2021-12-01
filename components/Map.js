82de589aa343960bf3f673a8b5b556f90542a5a2
Francesco <francesco.seddo@gmail.com>
2021-12-01 14:56:08 -0500

    minor fix with imports

--- components/Map.js
+++ components/Map.js
@@ -7,7 +7,6 @@ import {
   TouchableOpacity,
   ScrollView,
   Alert,
-  DrawerLayoutAndroidBase,
 } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
