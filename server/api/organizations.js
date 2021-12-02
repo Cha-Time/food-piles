7b1bcb9a6ed31e676a88fb8626fc378689a35013
Francesco <francesco.seddo@gmail.com>
2021-12-02 12:08:51 -0500

    added post route for orgs

--- server/api/organizations.js
+++ server/api/organizations.js
@@ -29,3 +29,31 @@ router.get("/:orgId", async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newOrg = await Organization.create({
+      name: req.body.name,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      zipCode: req.body.zipCode,
+      phoneNumber: req.body.phoneNumber,
+      accType: req.body.accType,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    });
+
+    let newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    });
+
+    newUser.setOrganization(newOrg);
+
+    res.json(newOrg);
+  } catch (error) {
+    next(error);
+  }
+});
