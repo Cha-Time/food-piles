ece6e0d8dc3e49456524619bb70478c971d1d0ac
Francesco <francesco.seddo@gmail.com>
2021-12-03 13:28:19 -0500

    removed extra code

--- server/api/favorites.js
+++ server/api/favorites.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  models: { User, favorites },
+  models: { User },
 } = require("../db");
 
 module.exports = router;
@@ -10,8 +10,6 @@ const { requireToken } = require("./authMiddleware");
 // GET for all organizatoins that this user follows// check to see if the current logged in user follows the ID specified here (user ID, connected to an organizatoin)
 router.get("/", requireToken, async (req, res, next) => {
   try {
-    const myUserId = req.user.id;
-
     const myUser = await User.findByPk(Number(req.user.id));
 
     const myFavorites = await myUser.getOrganizations();
@@ -23,26 +21,25 @@ router.get("/", requireToken, async (req, res, next) => {
 
 // if there is nothing found, we return nothing, which is interpreted on the other side as no
 
-router.get("/:orgId", requireToken, async (req, res, next) => {
-  try {
-    const myuserId = req.user.id;
-    const followOrgd = req.body.orgId;
-    const isFavorite = await favorites.findAll({
-      where: {
-        userId: myUserId,
-        followId: followCheckId,
-      },
-    });
-    res.json(favorites);
-  } catch (error) {
-    next();
-  }
-});
+// router.get("/:orgId", requireToken, async (req, res, next) => {
+//   try {
+//     const myUserId = req.user.id;
+//     const followOrgId = req.body.orgId;
+//     const isFavorite = await favorites.findAll({
+//       where: {
+//         userId: myUserId,
+//         followId: followOrgId,
+//       },
+//     });
+//     res.json(isFavorite);
+//   } catch (error) {
+//     next();
+//   }
+// });
 
 // POST - logged in user follows the ID specified
 router.post("/", requireToken, async (req, res, next) => {
   try {
-    const myUserId = req.user.id;
     const favoriteOrgId = req.body.orgId;
 
     const myUser = await User.findByPk(Number(req.user.id));
@@ -57,7 +54,6 @@ router.post("/", requireToken, async (req, res, next) => {
 // DELETE - logged in user unfollows the ID specified
 router.delete("/", requireToken, async (req, res, next) => {
   try {
-    const myUserId = req.user.id;
     const favoriteOrgId = req.body.orgId;
 
     const myUser = await User.findByPk(Number(req.user.id));
