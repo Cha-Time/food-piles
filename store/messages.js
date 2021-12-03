8f0960c2dc12216c4088cc0b8cf64b2416673a8f
lrschober <lawrence.schober@gmail.com>
2021-12-02 19:26:32 -0500

    new chat redux store api url

--- store/messages.js
+++ store/messages.js
@@ -1,39 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
-const GET_MESSAGES = 'GET_MESSAGES';
-const WRITE_MESSAGE = 'WRITE_MESSAGE';
-const GOT_NEW_MESSAGE_FROM_SERVER = 'GOT_NEW_MESSAGE_FROM_SERVER';
+const GET_MESSAGES = "GET_MESSAGES";
+const WRITE_MESSAGE = "WRITE_MESSAGE";
+const GOT_NEW_MESSAGE_FROM_SERVER = "GOT_NEW_MESSAGE_FROM_SERVER";
 
-const getMessages = messages => {
+const getMessages = (messages) => {
   return {
     type: GET_MESSAGES,
     messages,
   };
 };
 
-export const writeMessage = inputContent => {
+export const writeMessage = (inputContent) => {
   return {
     type: WRITE_MESSAGE,
     newMessageEntry: inputContent,
   };
 };
 
-export const gotNewMessageFromServer = message => {
+export const gotNewMessageFromServer = (message) => {
   return {
     type: GOT_NEW_MESSAGE_FROM_SERVER,
     message,
   };
 };
 
-export const fetchMessages = () => async dispatch => {
+export const fetchMessages = () => async (dispatch) => {
   // const { data: messages } = await axios.get('/api/messages');
-  const { data: messages } = await axios.get('http://192.168.1.7:8080/api/messages');
+  const { data: messages } = await axios.get(
+    "https://foodpiles.herokuapp.com/api/messages"
+  );
   dispatch(getMessages(messages));
 };
 
 const initialState = {
   messages: [],
-  NewMessageEntry: '',
+  NewMessageEntry: "",
 };
 
 export default (state = initialState, action) => {
