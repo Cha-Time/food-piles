32431e59c1600f3759ac81efb0d9a8b02880f4d2
lrschober <lawrence.schober@gmail.com>
2021-12-08 16:56:33 -0500

    Chat and ChatView starting to work, not broken yet...

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -9,20 +9,19 @@ import {
   Modal,
   Button,
   TextInput,
-} from 'react-native';
-import ChatView from './ChatView';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { fetchChats, setChats } from '../store/chats';
-import { fetchForeignOrganization } from '../store/singleForeignOrg';
+} from "react-native";
+import ChatView from "./ChatView";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { fetchChats, setChats } from "../store/chats";
+import { fetchForeignOrganization } from "../store/singleForeignOrg";
 
-export const Chat = props => {
-
+export const Chat = (props) => {
   const [visible, setVisible] = useState(false);
 
   const orgInfo = useSelector((state) => state.singleOrg);
 
   const dispatch = useDispatch();
-  let timeAgo = require('node-time-ago')
+  let timeAgo = require("node-time-ago");
 
   useEffect(() => {
     (async () => {
@@ -31,12 +30,18 @@ export const Chat = props => {
   }, [props.chats]);
 
   function toggleVisibility(status) {
-    setVisible(status)
+    setVisible(status);
   }
 
-  async function handleOnPress(orgId) {
-    await props.fetchOrganization(Number(orgId))
-    setVisible(true)
+  async function handleOnPress(orgId, orgInfo) {
+    props.navigation.navigate("ChatView", {
+      foreignId: orgId,
+      org: orgInfo,
+    });
+    /* visibleStatus={visible}
+          org={props.org}
+          receiverId={props.org.id}
+          toggleVisibility={toggleVisibility} */
   }
 
   return (
@@ -48,35 +53,58 @@ export const Chat = props => {
             <TouchableOpacity
               style={styles.listItem}
               onPress={() => {
-                handleOnPress(item.id)
+                handleOnPress(item.id, orgInfo);
               }}
             >
               <View style={styles.listItemView}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <Text numberOfLines={1} style={{ fontSize: 20, maxWidth: '50%' }}>{item.orgName}</Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text
+                    numberOfLines={1}
+                    style={{ fontSize: 20, maxWidth: "50%" }}
+                  >
+                    {item.orgName}
+                  </Text>
                   <Text numberOfLines={1} style={{ fontSize: 15 }}>
                     {`${
-                      ((Date.now() - new Date(item.msgTime).valueOf()) / 1000 / 60 / 60 / 24) > 1 
-                      ? 
-                      new Date(item.msgTime).toDateString() 
-                      :
-                      (
-                        ((Date.now() - new Date(item.msgTime).valueOf()) / 1000 / 60) < 1
-                        ?
-                        'just now'
-                        :
-                        timeAgo(item.msgTime)
-                      ) 
-                      }`}
+                      (Date.now() - new Date(item.msgTime).valueOf()) /
+                        1000 /
+                        60 /
+                        60 /
+                        24 >
+                      1
+                        ? new Date(item.msgTime).toDateString()
+                        : (Date.now() - new Date(item.msgTime).valueOf()) /
+                            1000 /
+                            60 <
+                          1
+                        ? "just now"
+                        : timeAgo(item.msgTime)
+                    }`}
                   </Text>
                 </View>
-                <Text numberOfLines={1} style={{ fontSize: 10 }}>{item.msg}</Text>
+                <Text numberOfLines={1} style={{ fontSize: 10 }}>
+                  {item.msg}
+                </Text>
               </View>
             </TouchableOpacity>
           </ScrollView>
         )}
       />
-      {visible === true ? (<ChatView visibleStatus={visible} org={props.org} receiverId={props.org.id} toggleVisibility={toggleVisibility} />) : (<View></View>)}
+      {/* {visible === true ? (
+        <ChatView
+          visibleStatus={visible}
+          org={props.org}
+          receiverId={props.org.id}
+          toggleVisibility={toggleVisibility}
+        />
+      ) : (
+        <View></View>
+      )} */}
     </View>
   );
 };
@@ -88,35 +116,35 @@ const styles = StyleSheet.create({
   },
   listItem: {
     padding: 15,
-    backgroundColor: '#f8f8f8',
+    backgroundColor: "#f8f8f8",
     borderBottomWidth: 2,
-    borderColor: '#eee',
+    borderColor: "#eee",
   },
   listItemView: {},
   btn: {
     marginRight: 315,
   },
   input: {
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
     padding: 10,
     margin: 10,
   },
 });
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     messages: state.messages,
     user: state.auth,
     chats: state.chats,
-    org: state.singleForeignOrg
+    org: state.singleForeignOrg,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     fetchChats: () => dispatch(fetchChats()),
-    fetchOrganization: (id) => dispatch(fetchForeignOrganization(id))
+    fetchOrganization: (id) => dispatch(fetchForeignOrganization(id)),
   };
 };
 
